b32fdc05d93922b2f599f7ffea7bfa1e900754eb
Joey Doyle <j.doyle345@gmail.com>
2017-11-14 08:22:25 -0800

    week7 day 2 homework

---
+++ week7/W7D2/solution_1/frontend/store/store.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import {applyMiddleWare} from 'react-redux';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(rootReducer, preloadedState);
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+}
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
+
+export default configureStore;
